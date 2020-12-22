01f7e3f111096833071d14e7692fe07bdd59f7dc
zhuxiaomei <865622793@qq.com>
2020-12-22 18:12:12 +0800

    complete video playing

--- MediaPlayer2/ViewController.swift
+++ MediaPlayer2/ViewController.swift
@@ -6,14 +6,70 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
+import AssetsLibrary
+import MobileCoreServices
 
-class ViewController: UIViewController {
+import PhotosUI
 
+class ViewController: UIViewController ,UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    func selectVideo(){
+        if(UIImagePickerController.isSourceTypeAvailable(.photoLibrary)){
+            let imagePicker = UIImagePickerController()
+            
+            imagePicker.delegate=self
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.mediaTypes = [kUTTypeMovie as String]
+            imagePicker.allowsEditing=false
+            present(imagePicker, animated: true, completion: nil)
+        }else{
+            print("error: read image error")
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let videoUrl = info[UIImagePickerController.InfoKey.referenceURL] as! URL
+        dismiss(animated: true, completion: nil)
+        playVideoWithAssetUrl(assetUrl: videoUrl)
+    }
+    
+    func playVideoWithAssetUrl(assetUrl:URL)  {
+        let authStatus = PHPhotoLibrary.authorizationStatus()
+        if authStatus == .notDetermined {
+            PHPhotoLibrary.requestAuthorization { (PHAuthorizationStatus) in
+                if(PHAuthorizationStatus == .authorized){
+                    self.doPlay(assetUrl: assetUrl)
+                }else{
+                    print("can't play")
+                }
+            }
+        }else if authStatus == .authorized {
+           doPlay(assetUrl: assetUrl)
+        }else {
+            print("can't play")
+        }
+    }
+    
+    func doPlay(assetUrl:URL)  {
+        DispatchQueue.main.async {
+            let asset = AVAsset(url: assetUrl)
+            let playItem = AVPlayerItem(asset: asset)
+            let player = AVPlayer(playerItem: playItem)
+            let playerController = AVPlayerViewController()
+            playerController.player = player
+            self.present(playerController, animated: true, completion: {
+                playerController.player!.play()
+            })
+        }
+    }
+    
+    @IBAction func btnClick(_ sender: UIButton) {
+        selectVideo()
+    }
 }
-
